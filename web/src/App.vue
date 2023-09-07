8dbc1cc5c74378ce4a6385badc7bbd00aad54f49
jc <2425796900@qq.com>
2023-09-07 13:18:23 +0800

    finish

---
+++ web/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <NavBar />
+  <router-view/>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+//import $ from 'jquery'
+export default{
+  components:{
+    NavBar
+  },
+  // setup(){
+   
+
+  //    $.ajax({
+  //     url:"http://127.0.0.1:3000/user/account/register/",
+  //     type:"post",
+  //     data:{
+  //       username:"cjc",
+  //       password:"jcc",
+  //       confirmedPassword:"jcc",
+
+        
+  //     },
+  //     success(resp){
+  //       console.log(resp);
+  //     },
+  //     error(resp){
+  //       console.log(resp);
+  //     },
+  //   });
+  // }
+}
+</script>
+<style>
+body{
+  background-image: url("@/assets/images/background.jpg");
+  background-size: cover;
+}
+</style>
---
+++ web/src/components/ContentField.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container content-field">
+        <div class="card">
+            <div class="card-body">
+                <slot></slot>
+                <!-- 填充的都在slot里面 -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+div.content-field{
+    margin-top: 20px;
+}
+div.card{
+    background:  rgba(0,0,0,0.0);
+    
+}
+
+</style>
